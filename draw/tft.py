655a8d93099a3fdc634201fef35a9bc216e4082c
tastatur <repkov.denis@gmail.com>
2015-01-11 17:53:35 +0100

    Performance improvements

--- draw/tft.py
+++ draw/tft.py
@@ -1,6 +1,5 @@
 __author__ = 'strah'
 from cberry import cberry
-import numpy
 import cv2
 import ctypes
 import Queue
@@ -27,16 +26,6 @@ class Tft:
             picture = self.drawqueue.get()
             # The picture needs to be resize for the TFT display first
             picture = cv2.resize(picture, (self.TFTW, self.TFTH))
-
-            (blue, green, red) = cv2.split(picture)
-            bmp = numpy.zeros(blue.shape, numpy.uint16)
-
-            # We have 5 bits for red and blue, and 6 bits for green
-            # See RA8770 datasheet for details
-            blue = cv2.normalize(blue, alpha=0, beta=31, norm_type=cv2.NORM_MINMAX)
-            green = cv2.normalize(green, alpha=0, beta=63, norm_type=cv2.NORM_MINMAX)
-            red = cv2.normalize(red, alpha=0, beta=31, norm_type=cv2.NORM_MINMAX)
-            for x, y in numpy.ndindex(blue.shape):
-                bmp[x][y] = (red[x][y] << 11) + (green[x][y] << 5) + blue[x][y]
+            bmp = cv2.cvtColor(picture, cv2.COLOR_BGR2BGR565, dstCn=1)
 
             cberry.RAIO_Write_Picture(bmp.ctypes.data_as(ctypes.POINTER(ctypes.c_uint16)), bmp.shape[0] * bmp.shape[1])

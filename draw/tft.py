6ad719b1e316cbfd090210b84286f9372d89c6b5
tastatur <repkov.denis@gmail.com>
2015-01-18 13:34:05 +0100

    Tft will be turned off after inactivity timeout, which can be set in property file

--- draw/tft.py
+++ draw/tft.py
@@ -4,6 +4,7 @@ import cv2
 import ctypes
 import Queue
 import threading
+import ConfigParser
 
 
 class Tft:
@@ -17,15 +18,32 @@ class Tft:
         cberry.TFT_hard_reset()
         cberry.RAIO_init()
         cberry.RAIO_clear_screen()
+        config = ConfigParser.RawConfigParser()
+        config.read('motion.properties')
+        self.timeout = config.getint("Tft", "screensaver")
+        self.on = True
         self.drawqueue = Queue.Queue()
         self.worker = threading.Thread(target=self.draw)
         self.worker.start()
 
     def draw(self):
         while True:
-            picture = self.drawqueue.get()
-            # The picture needs to be resize for the TFT display first
-            picture = cv2.resize(picture, (self.TFTW, self.TFTH))
-            bmp = cv2.cvtColor(picture, cv2.COLOR_BGR2BGR565, dstCn=1)
+            try:
+                picture = self.drawqueue.get(timeout=self.timeout)
+                # The picture needs to be resize for the TFT display first
+                picture = cv2.resize(picture, (self.TFTW, self.TFTH))
+                bmp = cv2.cvtColor(picture, cv2.COLOR_BGR2BGR565, dstCn=1)
+                if not self.on:
+                    self.stop_screensaver()
+                cberry.RAIO_Write_Picture(bmp.ctypes.data_as(ctypes.POINTER(ctypes.c_uint16)), bmp.shape[0] * bmp.shape[1])
+            except Queue.Empty:
+                if self.on:
+                    self.screensaver()
 
-            cberry.RAIO_Write_Picture(bmp.ctypes.data_as(ctypes.POINTER(ctypes.c_uint16)), bmp.shape[0] * bmp.shape[1])
+    def screensaver(self):
+        cberry.RAIO_power_off()
+        self.on = False
+
+    def stop_screensaver(self):
+        cberry.RAIO_power_on()
+        self.on = True
